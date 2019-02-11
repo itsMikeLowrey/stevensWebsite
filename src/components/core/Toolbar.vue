59501511379e50c812403c7161164d0c414673d8
itsMikeLowrey <itsMikeLowrey@users.noreply.github.com>
2019-02-11 14:53:04 -0800

    Page Title and logo change

--- src/components/core/Toolbar.vue
+++ src/components/core/Toolbar.vue
@@ -60,26 +60,8 @@
 </template>
 
 <script>
-  // Utilities
-  import {
-    mapGetters,
-    mapMutations
-  } from 'vuex'
 
   export default {
-    computed: {
-      ...mapGetters(['links'])
-    },
 
-    methods: {
-      ...mapMutations(['toggleDrawer']),
-      onClick (e, item) {
-        e.stopPropagation()
-
-        if (item.to || !item.href) return
-
-        this.$vuetify.goTo(item.href)
-      }
-    }
   }
 </script>
