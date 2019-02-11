640a4fd1d07bef5cab3945a2240a4118d7afd1f6
itsMikeLowrey <itsMikeLowrey@users.noreply.github.com>
2019-02-11 14:09:51 -0800

    Good Banner Now

--- src/components/home/Banner.vue
+++ src/components/home/Banner.vue
@@ -4,7 +4,7 @@
       class="grey lighten-2"
       height="400"
       width="100%"
-      :src="require('@/assets/articles/blurcamera.jpg')"
+      :src="require(`@/assets/banner.jpg`)"
     >
       <v-layout
         fill-height
@@ -12,23 +12,17 @@
         pa-3
       >
         <v-flex
-          xs12
-          md7
-          offset-md5
+          xs5
+          md5
+          offset-md6
+          style="background-color:rgba(67, 171, 245, 0.5)"
         >
           <h1 class="display-3 font-weight-light">
-            The Art Of Travel
+            Raising The Barr
           </h1>
           <div class="subheading text-uppercase pl-2 mb-4">
-            Finding Beauty, One flight at a time
+            A Modern Jack of All Trades
           </div>
-          <v-btn
-            color="primary"
-            depressed
-            round
-          >
-            Subscribe
-          </v-btn>
         </v-flex>
       </v-layout>
     </v-img>
