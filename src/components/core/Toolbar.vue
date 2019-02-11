df9983cbce24e3c033cb18629e2c58ecac206e6a
itsMikeLowrey <itsMikeLowrey@users.noreply.github.com>
2019-02-11 14:48:32 -0800

    More Changes

--- src/components/core/Toolbar.vue
+++ src/components/core/Toolbar.vue
@@ -1,21 +1,85 @@
-
 <template>
-
   <v-toolbar
     app
     flat
   >
-    <v-toolbar-title>Rasing The Bar</v-toolbar-title>
-    <v-toolbar-items class="hidden-sm-and-down">
-      <v-btn flat>Link One</v-btn>
-      <v-btn flat>Link Two</v-btn>
-      <v-btn flat>Link Three</v-btn>
-    </v-toolbar-items>
-    </v-toolbar>
+    <v-container
+      mx-auto
+      py-0
+    >
+      <v-layout>
+        <v-img
+          :src="require('@/assets/logo.png')"
+          class="mr-5"
+          contain
+          height="48"
+          width="48"
+          max-width="48"
+        />
+        <v-btn
+          class="ml-0 hidden-sm-and-down"
+          flat
+        >
+          Home
+        </v-btn>
+        <v-btn
+          class="ml-0 hidden-sm-and-down"
+          flat
+        >
+          Blog
+        </v-btn>
+        <v-btn
+          class="ml-0 hidden-sm-and-down"
+          flat
+        >
+          Shop
+        </v-btn>
+        <v-btn
+          class="ml-0 hidden-sm-and-down"
+          flat
+        >
+         Consultations
+        </v-btn>
+        <v-btn
+          class="ml-0 hidden-sm-and-down"
+          flat
+        >
+         portfolio
+        </v-btn>
+        <v-spacer />
+        <!-- <v-text-field
+          append-icon="mdi-magnify"
+          flat
+          hide-details
+          solo-inverted
+          style="max-width: 300px;"
+        /> -->
+      </v-layout>
+    </v-container>
+  </v-toolbar>
 </template>
 
 <script>
-/* eslint-disable */
+  // Utilities
+  import {
+    mapGetters,
+    mapMutations
+  } from 'vuex'
+
   export default {
+    computed: {
+      ...mapGetters(['links'])
+    },
+
+    methods: {
+      ...mapMutations(['toggleDrawer']),
+      onClick (e, item) {
+        e.stopPropagation()
+
+        if (item.to || !item.href) return
+
+        this.$vuetify.goTo(item.href)
+      }
+    }
   }
 </script>
